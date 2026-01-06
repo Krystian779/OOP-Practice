7310859015dca03a04bbd9caf2b742f8fb528cfc
Krystian Chmielak <S00271020@atu.ie>
2026-01-07 00:38:54 +0100

    Made Ticket Class

---
+++ OOP-Practice/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class Ticket
+    {
+        // Properties
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int AvailibleTickets { get; set; }
+
+        //Constructors
+
+        // Methods
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
