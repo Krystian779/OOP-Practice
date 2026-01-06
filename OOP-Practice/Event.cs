93afe29f7854e107bc07964a103f9cde657ed2fa
Krystian Chmielak <S00271020@atu.ie>
2026-01-07 00:44:32 +0100

    Added classes and IComparable

---
+++ OOP-Practice/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class Event: IComparable<Event>
+    {
+        public enum EventType
+        {
+            Music,
+            Comedy,
+            Theatre,
+            
+        }
+        // Properties
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public EventType TypeOfEvent { get; set; }
+
+        //Constructors
+        
+        // Methods
+        public int CompareTo(Event other)
+        {
+            return this.EventDate.CompareTo(other.EventDate);
+        }
+
+    }
+}
---
+++ OOP-Practice/VIPTicket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class VIPTicket:Ticket
+    {
+        // Properties
+        public string AdditionalExtras { get; set; }
+        public decimal AdditionalCost { get; set; }
+
+        //Constructors
+
+        // Methods
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
